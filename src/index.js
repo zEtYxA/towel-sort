221bf3062da1b0538ca62cbb8c526a98903c07fd
zEtYxA <zmaxet@mail.ru>
2022-08-09 00:31:47 +0300

    toel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined) { return [] }
+  let stuck = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 !== 0) {
+      stuck.push(matrix[i].reverse())
+    }
+    else {
+      stuck.push(matrix[i])
+    }
+  }
+  let stuck2 = [];
+  stuck.forEach(el => el.forEach(el => stuck2.push(el)))
+
+  return stuck2
 }
+
+//console.log(module.exports());
